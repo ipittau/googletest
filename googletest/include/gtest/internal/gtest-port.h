ac250e996d100123f8c1e34e7a315bcdb3ac4938
Ilario Pittau <ilario.pittau@abinsula.com>
2017-10-24 16:16:16 +0200

    DEATH: disable death test

--- googletest/include/gtest/internal/gtest-port.h
+++ googletest/include/gtest/internal/gtest-port.h
@@ -804,7 +804,7 @@ using ::std::tuple_size;
      (GTEST_OS_WINDOWS_DESKTOP && _MSC_VER >= 1400) || \
      GTEST_OS_WINDOWS_MINGW || GTEST_OS_AIX || GTEST_OS_HPUX || \
      GTEST_OS_OPENBSD || GTEST_OS_QNX || GTEST_OS_FREEBSD || GTEST_OS_NETBSD)
-# define GTEST_HAS_DEATH_TEST 1
+# define GTEST_HAS_DEATH_TEST 0
 #endif
 
 // We don't support MSVC 7.1 with exceptions disabled now.  Therefore
